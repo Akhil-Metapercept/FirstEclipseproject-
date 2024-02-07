35c420c2e00652df4da82ab24a14ba1258502479
Akhil-Metapercept <akhil.js@metapercept.com>
2024-02-07 16:46:07 +0530

    update code

--- src/Demo1/First.java
+++ src/Demo1/First.java
@@ -5,8 +5,8 @@ public class First {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Akhil");
-		System.out.println("Meta");
-		System.out.println("Percept");
+		System.out.println("Metapercept");
+		
 		
 		
 		
